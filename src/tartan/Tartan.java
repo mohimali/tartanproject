7d46338b3e332b9277d5dbe982506866e95eb758
Mohim Ali <mohim2k13@gmail.com>
2014-10-16 22:21:41 +0100

    Added some helper methods and constructor for Tartan

--- src/tartan/Tartan.java
+++ src/tartan/Tartan.java
@@ -5,17 +5,62 @@
  */
 package tartan;
 
+import java.awt.Color;
 /**
  *
  * @author mohim
  */
 public class Tartan {
+    
+   public int threadCount = 0;
+   public int threadSizes[];
+   public Color colours[];
+   public int settCount = 0;
+   public int warp = 0;
+   public int weft = 0;
+   
+   
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    private Tartan(int requiredThreadCount, int requiredThreadSizes[],
+                   Color requiredColours[], int requiredSettCount)
+    {
+        threadCount = requiredThreadCount;
+        threadSizes = requiredThreadSizes;
+        
+        settCount = requiredSettCount;
+        colours = requiredColours;
+        
+        
+    } // Constructor tartan
+    
+    public void updateThreadCount(int newThreadCount)
+    {
+        threadCount = newThreadCount;
+    } // updateThreadCount
+    
+    public void updateThreadSizes(int newThreadSizes[])
+    {
+        threadSizes = newThreadSizes;
+    } // updateThreadSizes
+    
+    public void updateColors(Color newColours[])
+    {
+        colours = newColours;
+    } // updateThreadSizes
+    
+    public void updateSettCount(int newSettCount)
+    {
+        settCount = newSettCount;
+    } // updateSettCount
+    
+    public int getThreadCount()
+    {
+        return threadCount;
+    } // getThreadCount
+    
+    public int getSettCount()
+    {
+        return settCount;
+    } // getSettCount
     
 }
